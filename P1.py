8f53fd6d5b1f5d401af6ff413bfbfc65237fc970
Daniel <danielsassman@gmail.com>
2016-03-21 14:28:11 +0000

    finished without comments

--- P1.py
+++ P1.py
@@ -1,9 +1,9 @@
 #get height of grid
-def get_height(grid):
+def height(grid):
 	return (len(grid))
 
 #get width of grid
-def get_width(grid):
+def width(grid):
 	return (len(grid[0]))
 
 	
@@ -12,9 +12,9 @@ def energy_at(grid,r,c):
 	return (width_energy(grid, r, c) + height_energy(grid,r,c))
 
 def energy(grid):
-	newGrid = [[0 for x in range(get_width(grid))]for x in range(get_height(grid))]
-	for i in range(get_height(grid)):
-		for j in range (get_width(grid)):
+	newGrid = [[0 for x in range(width(grid))]for x in range(height(grid))]
+	for i in range(height(grid)):
+		for j in range (width(grid)):
 			newGrid[i][j] = energy_at(grid, i , j)
 	return newGrid
 
@@ -30,11 +30,11 @@ def get_blue(grid, r, c):
 #calculate width energy
 def width_energy(grid, r, c):
 	if(c==0):
-		red = (get_red(grid, r, (get_width(grid)-1)) - get_red(grid, r, (c+1)))
-		green = (get_green(grid, r, (get_width(grid)-1)) - get_green(grid, r, (c+1)))
-		blue = (get_blue(grid, r, (get_width(grid)-1)) - get_blue(grid, r, (c+1)))
+		red = (get_red(grid, r, (width(grid)-1)) - get_red(grid, r, (c+1)))
+		green = (get_green(grid, r, (width(grid)-1)) - get_green(grid, r, (c+1)))
+		blue = (get_blue(grid, r, (width(grid)-1)) - get_blue(grid, r, (c+1)))
 
-	elif(c==get_width(grid)-1):
+	elif(c==width(grid)-1):
 		red = (get_red(grid, r, (c-1)) - get_red(grid, r, 0))
 		green = (get_green(grid, r, (c-1)) - get_green(grid, r, 0))
 		blue = (get_blue(grid, r, (c-1)) - get_blue(grid, r, 0))
@@ -49,10 +49,10 @@ def width_energy(grid, r, c):
 #calculate height energy
 def height_energy(grid, r, c):
 	if(r==0):
-		red = (get_red(grid, (get_height(grid)-1), c) - get_red(grid, (r+1), c))
-		green = (get_green(grid, (get_height(grid)-1), c) - get_green(grid, (r+1), c))
-		blue = (get_blue(grid, (get_height(grid)-1), c) - get_blue(grid, (r+1), c))
-	if(r==get_height(grid)-1):
+		red = (get_red(grid, (height(grid)-1), c) - get_red(grid, (r+1), c))
+		green = (get_green(grid, (height(grid)-1), c) - get_green(grid, (r+1), c))
+		blue = (get_blue(grid, (height(grid)-1), c) - get_blue(grid, (r+1), c))
+	if(r==height(grid)-1):
 		red = (get_red(grid, (r-1), c) - get_red(grid, 0, c))
 		green = (get_green(grid, (r-1), c) - get_green(grid, 0, c))
 		blue = (get_blue(grid, (r-1), c) - get_blue(grid, 0, c))
@@ -69,65 +69,136 @@ def ret_element(grid,r,c):
 	print(grid[r][c])
 
 
-#FINDING PATHS
+
 def find_vertical_path(grid):
-	vertCoords = [(0,0) for x in range(get_height(grid))]
+	vertCoords = [(0,0) for x in range(height(grid))]
+	totals = [0 for x in range(width(grid))]
+	totalCount = 0
 	vertGrid = energy(grid)
+	totalEnergy = 0
 	lowest = vertGrid[0][0]
 	k = 1
-	c = 0 #setting column number
-	for i in range (get_width(grid)-1): #finding lowest in top row
-		if(vertGrid[0][i]) < lowest:
-			lowest = vertGrid[0][i]
-			vertCoords[0] = (0,i)
-			c = i
-	while k < (get_height(grid)): #moving down the grid
-		nextCoord = vertGrid[k][c]
-		if c > 0 and vertGrid[k][c-1] < nextCoord:
-			c = c-1
-			pass
-		elif c < (get_width(grid)-1) and vertGrid[k][c+1] < nextCoord:
-			c = c+1
-		vertCoords[k] = (k,c)
-		k = k + 1			 
+	totalReturn = 0
+	up = 0
+	r = 0 #setting row number
+	
+	for h in range (width(grid)):
+		r = h
+		vertCoords[0] = (0,h)
+		for k in range (1,height(grid)): #moving across the grid
+			nextCoord = vertGrid[k][r]
+			if r > 0 and vertGrid[k][r-1] <= nextCoord:
+				if r < (width(grid)-1) and vertGrid[k][r+1] < vertGrid[k][r-1]:
+					r = r + 1
+					up = 1
+				else:
+					r = r-1
+			elif r < (width(grid)-1) and  vertGrid[k][r+1] < nextCoord and up < 1:
+				r = r+1
+			up = 0
+			vertCoords[k] = (k,r)
+			k = k + 1
+		
+		for l in range(height(grid)):
+			totalEnergy = totalEnergy + int(energy_at(grid, int(vertCoords[l][0]), int(vertCoords[l][1])))
+		totals[totalCount] = totalEnergy
+		totalCount = totalCount + 1
+		totalEnergy = 0
+		vertCoords = [(0,0) for x in range(height(grid))]
+	minValue = totals[0]
+	for p in range (width(grid)):
+		if(totals[p] < minValue):
+			totalReturn = p
+			minValue = totals[p]
+	vertCoords[0] = (0, totalReturn)
+	r = totalReturn						 
+	for k in range (1,height(grid)): #moving across the grid
+		nextCoord = vertGrid[k][r]
+		if r > 0 and vertGrid[k][r-1] <= nextCoord:
+			if r < (width(grid)-1) and vertGrid[k][r+1] < vertGrid[k][r-1]:
+				r = r + 1
+				up = 1
+			else:
+				r = r-1
+		elif r < (width(grid)-1) and vertGrid[k][r+1] < nextCoord and up < 1:
+			r = r+1
+		up = 0
+		vertCoords[k] = (k,r)
+		k = k + 1
+		
 	return vertCoords
 
-
 def find_horizontal_path(grid):
-	horCoords = [(0,0) for x in range(get_width(grid))]
+	horCoords = [(0,0) for x in range(width(grid))]
+	totals = [0 for x in range(height(grid))]
+	totalCount = 0
 	horGrid = energy(grid)
+	totalEnergy = 0
 	lowest = horGrid[0][0]
 	k = 1
+	totalReturn = 0
+	up = 0
 	r = 0 #setting row number
-	for i in range (get_height(grid)-1): #finding lowest in top row
-		if(horGrid[i][0]) < lowest:
-			lowest = horGrid[i][0]
-			horCoords[0] = (i,0)
-			r = i
-	while k < (get_width(grid)): #moving across the grid
+	
+	for h in range (height(grid)):
+		r = h
+		horCoords[0] = (h,0)
+		for k in range (1,width(grid)): #moving across the grid
+			nextCoord = horGrid[r][k]
+			if r > 0 and horGrid[r-1][k] <= nextCoord:
+				if r < (height(grid)-1) and horGrid[r+1][k] < horGrid[r-1][k]:
+					r = r + 1
+					up = 1
+				else:
+					r = r-1
+			elif r < (height(grid)-1) and horGrid[r+1][k] < nextCoord and up < 1:
+				r = r+1
+			up = 0
+			horCoords[k] = (r,k)
+			k = k + 1
+		
+		for l in range(width(grid)):
+			totalEnergy = totalEnergy + int(energy_at(grid, int(horCoords[l][0]), int(horCoords[l][1])))
+		totals[totalCount] = totalEnergy
+		totalCount = totalCount + 1
+		totalEnergy = 0
+		horCoords = [(0,0) for x in range(width(grid))]
+	minValue = totals[0]
+	for p in range (height(grid)):
+		if(totals[p] < minValue):
+			totalReturn = p
+			minValue = totals[p]
+	horCoords[0] = (totalReturn, 0)
+	r = totalReturn						 
+	for k in range (1,width(grid)): #moving across the grid
 		nextCoord = horGrid[r][k]
-		if r > 0 and horGrid[r-1][k] < nextCoord:
-			r = r-1
-			pass
-		elif r < (get_height(grid)-1) and horGrid[r+1][k] < nextCoord:
+		if r > 0 and horGrid[r-1][k] <= nextCoord:
+			if r < (height(grid)-1) and horGrid[r+1][k] < horGrid[r-1][k]:
+				r = r + 1
+				up = 1
+			else:
+				r = r-1
+		elif r < (height(grid)-1) and horGrid[r+1][k] < nextCoord and up < 1:
 			r = r+1
+		up = 0
 		horCoords[k] = (r,k)
-		k = k + 1			 
+		k = k + 1
+		
 	return horCoords
 
 def remove_vertical_path(grid, path):
-	for i in range (get_height(grid)):
+	for i in range (height(grid)):
 		del grid[(path[i][0])][(path[i][1])]
 	return grid
 
 
 def remove_horizontal_path(grid, path):
-	height = get_height(grid)
-	width = get_width(grid)
-	for i in range (get_width(grid)):
+	#height = height(grid)
+	#width = width(grid)
+	for i in range (width(grid)):
 		grid[(path[i][0])][(path[i][1])] = (-1,i,-1)
 	grid = list(map(list,zip(*grid))) #transposing grid
-	for k in range (get_height(grid)):
+	for k in range (height(grid)):
 		del grid[(path[k][1])][(path[k][0])]
 	grid = list(map(list,zip(*grid))) #returning grid to normal state
 	return grid
@@ -148,33 +219,18 @@ def read_lines(filename):
 			data2[r-y] = data[r]
 		else:
 			y += 1
-	#print(len(data2[0]))
-	#print(data2[1])
-	#print(data2[2])
-	#print(data2[3])
-	#print(data2[4])
-	#print(data2[len(data2)-2])
-	#print(data2[len(data2)-1])
 	return data2
 
 def ppm_to_grid(filename):
 	data = read_lines(filename)
 	rgbstring = ""
-	rowcol = data[1].split()
-	row = rowcol[0]
-	col = rowcol[1]
-	print("type = " + data[0])
-	print("row = " + row)
-	print("col = " + col)
-	print("max length = " + data[2])
-	print("data length = " + str(len(data)))
-	for k in range(3,len(data)):
+	col = data[1]
+	row = data[2]
+	maxValue = data[3]
+	for k in range(4,len(data)):
 		rgbstring += str(data[k])
 	data2 = rgbstring.split()
-	#print(data2)
-	print("data2 " + str(len(data2)))
 	rgblength = int(row) * int(col)
-	print("rgb length = " + str(rgblength))
 	datacount = -1
 	
 
@@ -186,51 +242,74 @@ def ppm_to_grid(filename):
 		num2 = data2[datacount]
 		datacount += 1
 		num3 = data2[datacount]
-		rgbcoords[k] = (num1, num2, num3)
-	print("rgbcoords length = " + str(len(rgbcoords)))
+		rgbcoords[k] = (int(num1), int(num2), int(num3))
 	
 	#HAVE TO SEPERATE BY ROWS. PUTTING EACH ROW INTO A INDEX OF A 2D LIST
 	finalMatrix = [[0 for x in range(int(col))] for x in range(int(row))]
-	print("final matrix length = " + str(len(finalMatrix)))
-	print("final matrix2 length = " + str(len(finalMatrix[0])))
-	print("rgbcoords[0] = " + str(rgbcoords[0]))
 	colCount = 0
 	rowCount = 0
 	
-	for h in range(len(rgbcoords)-1):
+	for h in range(len(rgbcoords)):
 		
 		finalMatrix[rowCount][colCount] = rgbcoords[h]
-		#print("finalmatrix: " + str(finalMatrix[rowCount][colCount]));
-		#print(h)
-		print("h = " + str(h) + " row = " + str(rowCount) + " col = " + str(colCount))
-		#print(colCount)
 		colCount += 1
 		if(colCount==(int(col))):
 			rowCount += 1
 			colCount = 0
-	print("rgbcoords = " + str(len(rgbcoords)))
-	print("finalmatrix[0] = " + str(finalMatrix[0]))
-	print("length finalmatrix[0] = " + str(len(finalMatrix[0])))
-	print("finalmatrix[10] = " + str(finalMatrix[10]))
-	#return finalMatrix		
-	#return rgbcoords
-	#print(rgbcoords[0])
-	#print(rgbcoords[1])
-	#print(rgbcoords[tupleCount-1])
+	return finalMatrix		
 	
+
+def grid_to_ppm(grid, filename):
+	f = open(filename, 'w')
+	f.write('P3\n')
+	f.write(str(width(grid)) + '\n')
+	f.write(str(height(grid)) + '\n')
+	f.write('255\n')
+	totalCoords = height(grid) * width(grid) * 3
+	for k in range(height(grid)):
+		for h in range(width(grid)):
+			f.write(str(grid[k][h][0])+'\n')		
+			f.write(str(grid[k][h][1])+'\n')	
+			f.write(str(grid[k][h][2])+'\n')
+	f.close()		
+	
+
+
+
+#FOR TESTING???????????????????????????????????????????????????????????????????????????
+def make_file(msg,filename=".deletable_temp_file.txt"):
+	f = open(filename,'w')
+	f.write(msg)
+	f.close()
+
+def from_file(filename):
+	f = open (filename)
+	s = f.read()
+	f.close()
+	return s
+
+def g2():
+	return [[( 78, 209,  79), ( 63, 118, 247), ( 92, 175,  95), (243,  73, 183), (210, 109, 104), (252, 101, 119)],
+	      [(224, 191, 182), (108,  89,  82), ( 80, 196, 230), (112, 156, 180), (176, 178, 120), (142, 151, 142)],
+	      [(117, 189, 149), (171 ,231, 153), (149, 164, 168), (107, 119,  71), (120, 105, 138), (163, 174, 196)],
+	      [(163, 222, 132), (187 ,117, 183), ( 92, 145,  69), (158, 143,  79), (220,  75, 222), (189,  73, 214)],
+	      [(211, 120, 173), (188 ,218, 244), (214, 103,  68), (163, 166, 246), ( 79, 125, 246), (211, 201,  98)]
+	     ]
+
+def g5():
+	return [
+		[(0,0,0),(0,0,0),(10,20,30),(0,0,0),(0,0,0),(0,0,0)],
+		[(0,0,0),(2,3,4),( 1, 1, 1),(5,6,7),(0,0,0),(0,0,0)],
+		[(0,0,0),(0,0,0),(60,50,40),(0,0,0),(0,0,0),(0,0,0)],
+		[(0,0,0),(0,0,0),( 0, 0, 0),(0,0,0),(0,0,0),(0,0,0)]
+		]
 
 def main():
-
-	
-	p1 = [
-      [(100, 75,200), (100,100,200), (100,100,200), (100,100,200), (200,125,200)],
-      [(150, 30,180), (150, 50,180), (100,120,180), (100,120,180), (100,120,180)],
-      [(100, 75,100), (100, 80,100), (100, 85,100), (100, 95,100), (100,110,100)],
-      [(200,100, 10), (200,100, 10), (200,100, 10), (210,200, 10), (255,  0, 10)]
-     ]
-	path = find_horizontal_path(p1)
-	print(path)
-	print(p1)
-	print()
-	p1 = remove_horizontal_path(p1,path)
-	print(p1)
+	#print(energy_at(g2(), 0 ,0))
+	print(energy(g2()))
+	print(find_vertical_path(g2()))
+	#filename = ".temp_file.ppm"
+	#ppm = grid_to_ppm(g2(),filename)
+	#s = from_file(filename)
+	#tokens = s.split()
+	#print(tokens)
